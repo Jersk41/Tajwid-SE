fee1c1926bac349a35939e57ff7ac86b96238ec8
Jersk41 <japarssidik820@gmail.com>
2021-01-07 12:00:32 +0700

    add idghom bilagunnah regex

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
+// regex hukum izhar
 let izhar = /(?:(نْ|ً|ٌ|ٍ)((ء|ئ|ؤ|أ|ء)|ه|ع|ح|غ|خ)|(ً|ٍ|ٌ|نْ).((ئ|ؤ|أ|ء|ا)|ه|ع|ح|غ|خ))/mgi
-let coba1 = "سَلَامٌ هِيَ";
-let coba2 = "مِنْ عَيْنٍ";
-let coba3 = "وَمِنْ حَيْثُ";
-/* 
-console.log(coba1.match(izhar));
-console.log(coba2.match(izhar));
-console.log(coba3.match(izhar));
- */
-// implementasi hukum izhar
+
+// regex hukum idhgom bila gunnah v2 enchanted
+let bilaGunnah = /((?:(نْ|ً|ٌ|ٍ).(ل|ر))|(?:(نْ|ً|ٌ|ٍ)ا.(ر|ل)))/mgi
+
+// implementasi hukum
 let raw = document.querySelector("#rawWord").value;
-let hasilPencarian = raw.match(izhar);
+let hasilPencarian = raw.match(bilaGunnah);
 console.log(raw);
 console.log(hasilPencarian);
-// raw.search(izhar);
-// let splited = raw.split(hasilPencarian);
-// console.log(splited);
-document.querySelector("#hasil").innerHTML = `Dari lafadz ${raw} ditemukan hukum izhar karna memiliki ciri berikut`;
+document.querySelector("#hasil").innerHTML = `Input user : ${raw} Hasilnya : `;
 document.querySelector("#arab").innerHTML  = hasilPencarian;
-// console.log(sliced.concat("<tajwid data-id='1'>"));
