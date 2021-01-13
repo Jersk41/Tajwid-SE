d5d5a11b03a66354f78ed750b017a3374194353f
Jersk41 <japarssidik820@gmail.com>
2021-01-13 18:11:46 +0700

    all function relate to databases

--- function.php
+++ function.php
@@ -1,5 +1,33 @@
 <?php
-// koneksi
-$conn = new mysqli("localhost","root","","db_tajwid");
-$conn->set_charset('utf8');
+class Db
+{
+    public $conn;
+    // mengkonstuksi koneksi
+    public function __construct() {
+        $this->conn = new mysqli("localhost","root","","db_tajwid");
+        $this->conn->set_charset('utf8');
+        return $this->conn;
+    }
+
+    public function read()
+    {
+        $data = [];
+        $result = $this->conn->query("SELECT h.id_huruf,h.nama_huruf,h.hijaiyah,m.nama_makhroj,m.tempat_makhroj,m.arti_makhroj
+        FROM huruf h, makhroj m
+        WHERE h.makhroj=m.id_makhroj");
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+    public function readSpesific($parameter,$condition)
+    {
+        $data = [];
+        $result = $this->conn->query("SELECT $parameter FROM sifat WHERE $condition");
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+}
 ?>
