a3a0e60a42f59db8fee7f062be4da18bec632c86
Jersk41 <japarssidik820@gmail.com>
2021-01-14 14:38:09 +0700

    use cdn for resources css and js

--- response.php
+++ response.php
@@ -7,11 +7,9 @@
     <title>Hijaiyah Search Enginer</title>
     <link rel="stylesheet" href="style.css">
     <!-- framework -->
-    <script src="dist/js/jquery-3.5.1.min.js"></script>
-	<link href="dist/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="dist/js/bootstrap.min.js"></script>
-	<!------ Include the above in your HEAD tag ---------->
-
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
 		integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
